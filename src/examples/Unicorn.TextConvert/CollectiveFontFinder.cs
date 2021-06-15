bee45d1eeab20643da4085235d51b504e4ad70fd
Will Salt <forestpines@gmail.com>
2021-06-15 22:01:28 +0100

    Issue #68 Select font

---
+++ src/examples/Unicorn.TextConvert/CollectiveFontFinder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Unicorn.Base;
+
+namespace Unicorn.TextConvert
+{
+    public class CollectiveFontFinder : IFontFinder, IDisposable
+    {
+        private readonly TruetypeFontFinder _ttfFinder;
+        private readonly StandardFontFinder _standardFinder;
+        private bool _disposed;
+
+        public CollectiveFontFinder(string defaultFontFolder)
+        {
+            _ttfFinder = new TruetypeFontFinder(defaultFontFolder);
+            _standardFinder = new StandardFontFinder();
+        }
+
+        public IFontDescriptor FindFont(string name, double size) => _ttfFinder.FindFont(name, size) ?? _standardFinder.FindFont(name, size);
+
+        #region IDisposable implementation
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _ttfFinder.Dispose();
+                }
+
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
+    }
+}
